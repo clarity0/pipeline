dae5d41ca5eeb6b7b8397973bcfc50b1af32039a
clarity <clarity96@mailfence.com>
2022-04-09 16:16:35 -0300

    good poc

---
+++ src/download.rs
@@ -0,0 +1,10 @@
+use ytdl::*;
+
+pub fn download_link(dl_link: impl AsRef<str>) -> YoutubeDlOutput {
+	eprintln!("link: {}", dl_link.as_ref());
+	YoutubeDl::new(dl_link.as_ref())
+		.youtube_dl_path("yt-dlp")
+        .download(true)
+        .run()
+        .unwrap()
+}
